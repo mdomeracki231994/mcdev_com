cf4bfb6b7c287247c3fa2634a590adc11fdeec24
mark <mdomeracki231994>
2022-06-03 12:48:01 -0400

    Add landing page index

--- landing_pages/urls.py
+++ landing_pages/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . views import *
 
 urlpatterns = [
+    path('landing_page_index', index, name='links_page'),
     path('construction/', construction, name='construction'),
     path('restaurant/', restaurant, name='restaurant'),
     path('mechanics/', mechanics, name='mechanics'),
--- landing_pages/views.py
+++ landing_pages/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 
+def index(request):
+    return render(request, 'landing_pages/index.html')
+
+
 def construction(request):
     return render(request, 'landing_pages/construction/construction.html')
 
