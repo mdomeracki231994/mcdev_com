5e2bf656dc357f0fec810b5fde660080be040232
mark <mdomeracki231994>
2022-06-08 12:17:04 -0400

    Add date time stamp for forms

--- home/models.py
+++ home/models.py
@@ -3,3 +3,5 @@ from django.db import models
 
 class Newsletter(models.Model):
     email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
