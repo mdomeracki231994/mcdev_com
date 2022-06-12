f22dd16d03dd5698c32203cbae2403198cadc7d2
mdomeracki231994 <domerackimark23@gmail.com>
2022-06-11 22:39:24 -0400

    Adding alerts for when someone fills out the contact form

--- utils/common/send_email.py
+++ utils/common/send_email.py
@@ -1,3 +1,20 @@
+import smtplib
 
 
+def mail_send(name, email, phone, message):
+    with smtplib.SMTP('mail.bluetreeconcepts.com', 587) as smtp:
+        notify_list = ['domerackimark@gmail.com', 'itcfl@yahoo.com', 'mark.domeracki@bluetreeconcepts.com']
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login('info@bluetreeconcepts.com', 'Usmc9423#')
 
+        msg = f"""From: info@bluetreeconcepts.com\nSubject: test subject\n\n 
+        {name}
+        {email}
+        {phone}
+        {message}
+        """
+
+        smtp.sendmail('info@bluetreeconcepts.com', notify_list, msg)
+        print('sent')
